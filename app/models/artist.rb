2d78bba4a276a81e0e74fe30a7e7c9808743d18f
YutoKanai <tys7768@gmailcom>
2018-10-16 23:13:08 +0900

    直近の、アーティストがOK状態のイベントの日程を出す

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -43,6 +43,22 @@ class Artist < ApplicationRecord
     offers.find_by(event: event)
   end
 
+  def nearest_event
+    events.minimum("date").strftime("%m/%d")
+  end
+
+  def nearest_event_id
+    events.order(:date).select("id").first
+  end
+
+  def nearest_ok_event
+    @artist = Artist.all.sample
+    events =  @artist.events
+    offer_events = events.where(offers: @artist.offers)
+    nearest_event = offer_events.order('date asc').first
+    nearest_event.date.strftime("%m/%d")
+  end
+
   # aasm_state_artists でofferの状態がaasm_stateのartists一覧を取得できる Event.ok_artists
   Offer.aasm.states.map(&:name).each do |state|
     define_method("#{state}_events".to_sym) do
